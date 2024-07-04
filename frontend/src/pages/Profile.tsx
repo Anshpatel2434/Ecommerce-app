256ae5c8b44be53f903bf10f65a048e198e5638c
DaVinci <siddharthkotak280@gmail.com>
2024-07-04 17:21:24 +0530

    Profile page added

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import ProfileSection from "../components/ProfileSection";
+import Footer from "../components/Footer";
+
+const Profile = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col overflow-x-hidden">
+      <Navbar />
+      <ProfileSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile;
