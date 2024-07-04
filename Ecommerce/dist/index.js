634ef1dbc53ab3953b5dd12d77d3dd351175cbb4
Anshpatel2434 <anshpatel2434@gmail.com>
2024-07-04 17:15:06 +0530

    added backend for items

--- Ecommerce/dist/index.js
+++ Ecommerce/dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.signInInput = exports.signUpInput = void 0;
+exports.itemInput = exports.signInInput = exports.signUpInput = void 0;
 const zod_1 = require("zod");
 exports.signUpInput = zod_1.z.object({
     name: zod_1.z.string().min(1, { message: "Name cannot be empty" }),
@@ -10,8 +10,15 @@ exports.signUpInput = zod_1.z.object({
         .min(8, { message: "Password should atleast be of 8 characters" }),
 });
 exports.signInInput = zod_1.z.object({
-    email: zod_1.z.string().min(1, { message: "Invalid email type" }),
+    email: zod_1.z.string().email({ message: "Invalid email type" }),
     password: zod_1.z
         .string()
         .min(8, { message: "Password should atleast be of 8 characters" }),
+});
+exports.itemInput = zod_1.z.object({
+    category: zod_1.z.string().min(1, { message: "Category cannot be empty" }),
+    itemName: zod_1.z.string().min(1, { message: "Item Name cannot be empty" }),
+    itemPrice: zod_1.z.number().min(1, { message: "Item Price cannot be empty" }),
+    itemQuantity: zod_1.z.number().min(1, { message: "Item Quantity cannot be empty" }),
+    itemImage: zod_1.z.string().optional(),
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { userRouter } from "./routes/user";
+import { itemRouter } from "./routes/items";
 
 const app = new Hono();
 
 app.use("/*", cors());
 
 app.route("/api/v1/user", userRouter);
+app.route("/api/v1/item", itemRouter);
 
 export default app;
