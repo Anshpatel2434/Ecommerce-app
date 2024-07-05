ebbfe9baf4e29a4d8fa3ca28117542d64ad1c7c5
Anshpatel2434 <anshpatel2434@gmail.com>
2024-07-05 19:37:50 +0530

    removed 2 country names in profile

--- frontend/src/components/ProfileSection.tsx
+++ frontend/src/components/ProfileSection.tsx
@@ -7,157 +7,154 @@ import Loading from "./Loading";
 import { useNavigate } from "react-router-dom";
 
 const ProfileSection = () => {
-  const navigate = useNavigate();
-  const { profile, loading, setProfile, status } = useProfile();
-  const { user } = useContext(AppContext) as Context;
-  const name = user.name;
+	const navigate = useNavigate();
+	const { profile, loading, setProfile, status } = useProfile();
+	const { user } = useContext(AppContext) as Context;
+	const name = user.name;
 
-  useEffect(() => {
-    setProfile(profile);
-  }, [loading]);
+	useEffect(() => {
+		setProfile(profile);
+	}, [loading]);
 
-  return (
-    <>
-      {!loading ? (
-        <>
-          {status === 200 ? (
-            <>
-              {user.name !== "" ? (
-                <section className="bg-gray-900 mt-16 flex font-medium items-center justify-center min-h-screen">
-                  <section className="mx-auto bg-gray-800 rounded-2xl flex flex-col justify-center items-center px-8 py-6 shadow-2xl w-full max-w-md md:max-w-lg lg:max-w-xl">
-                    <div className="mt-6 w-fit mx-auto">
-                      <div className="rounded-full font-playwrite flex items-center justify-center text-3xl text-white w-28 h-28 border-4 border-blue-500">
-                        {name[0]}
-                      </div>
-                    </div>
+	return (
+		<>
+			{!loading ? (
+				<>
+					{status === 200 ? (
+						<>
+							{user.name !== "" ? (
+								<section className="bg-gray-900 mt-16 flex font-medium items-center justify-center min-h-screen">
+									<section className="mx-auto bg-gray-800 rounded-2xl flex flex-col justify-center items-center px-8 py-6 shadow-2xl w-full max-w-md md:max-w-lg lg:max-w-xl">
+										<div className="mt-6 w-fit mx-auto">
+											<div className="rounded-full font-playwrite flex items-center justify-center text-3xl text-white w-28 h-28 border-4 border-blue-500">
+												{name[0]}
+											</div>
+										</div>
 
-                    <div className="mt-8 text-center">
-                      <h2 className="text-white font-bold text-2xl tracking-wide">
-                        {name}
-                      </h2>
-                    </div>
+										<div className="mt-8 text-center">
+											<h2 className="text-white font-bold text-2xl tracking-wide">
+												{name}
+											</h2>
+										</div>
 
-                    <div className="mt-4 w-full">
-                      <table className="text-white text-xl tracking-wide w-full">
-                        <tbody>
-                          <tr className="mt-4 flex items-center">
-                            <td className="flex items-center">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">Country:</span>
-                            </td>
-                            <td>
-                              {profile?.country}
-                              {profile?.country}
-                            </td>
-                          </tr>
-                          <tr className="mt-4 flex items-center">
-                            <td className="flex items-center">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">State:</span>
-                            </td>
-                            <td>{profile?.state}</td>
-                          </tr>
-                          <tr className="mt-4 flex items-center">
-                            <td className="flex items-center">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">City:</span>
-                            </td>
-                            <td>{profile?.city}</td>
-                          </tr>
-                          <tr className="mt-4 flex items-center">
-                            <td className="flex items-center">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">Pincode:</span>
-                            </td>
-                            <td>{profile?.pincode}</td>
-                          </tr>
-                          <tr className="mt-4 flex items-center">
-                            <td className="flex items-center">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">Phone no.:</span>
-                            </td>
-                            <td>{profile?.phone}</td>
-                          </tr>
-                          <tr className="mt-4 flex flex-col break-all">
-                            <td className="flex items-center mb-2">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">Email:</span>
-                            </td>
-                            <td className="flex">
-                              {" "}
-                              <FaArrowRight
-                                className="mr-2 mt-[0.3rem]"
-                                size={20}
-                              />{" "}
-                              {user.email}
-                            </td>
-                          </tr>
-                          <tr className="mt-4 flex flex-col break-all whitespace-normal">
-                            <td className="flex items-center mb-2">
-                              <FaRegCircleDot
-                                fill="white"
-                                size={20}
-                                className="mt-[0.3rem] mr-3"
-                              />
-                              <span className="text-2xl">Address:</span>
-                            </td>
-                            <td className="flex">
-                              <FaArrowRight
-                                className="mr-2 mt-[0.3rem]"
-                                size={25}
-                              />{" "}
-                              {profile?.address}
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </section>
-                </section>
-              ) : (
-                <Loading />
-              )}
-            </>
-          ) : (
-            <div className="w-full h-full flex items-center justify-center bg-gray-800">
-              <button
-                onClick={() => navigate("/setProfile")}
-                className="rounded-lg text-white bg-black flex justify-center items-center border-white border-2 text-xl font-semibold p-8"
-              >
-                Set your Profile
-              </button>
-            </div>
-          )}
-        </>
-      ) : (
-        <Loading />
-      )}
-    </>
-  );
+										<div className="mt-4 w-full">
+											<table className="text-white text-xl tracking-wide w-full">
+												<tbody>
+													<tr className="mt-4 flex items-center">
+														<td className="flex items-center">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">Country:</span>
+														</td>
+														<td>{profile?.country}</td>
+													</tr>
+													<tr className="mt-4 flex items-center">
+														<td className="flex items-center">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">State:</span>
+														</td>
+														<td>{profile?.state}</td>
+													</tr>
+													<tr className="mt-4 flex items-center">
+														<td className="flex items-center">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">City:</span>
+														</td>
+														<td>{profile?.city}</td>
+													</tr>
+													<tr className="mt-4 flex items-center">
+														<td className="flex items-center">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">Pincode:</span>
+														</td>
+														<td>{profile?.pincode}</td>
+													</tr>
+													<tr className="mt-4 flex items-center">
+														<td className="flex items-center">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">Phone no.:</span>
+														</td>
+														<td>{profile?.phone}</td>
+													</tr>
+													<tr className="mt-4 flex flex-col break-all">
+														<td className="flex items-center mb-2">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">Email:</span>
+														</td>
+														<td className="flex">
+															{" "}
+															<FaArrowRight
+																className="mr-2 mt-[0.3rem]"
+																size={20}
+															/>{" "}
+															{user.email}
+														</td>
+													</tr>
+													<tr className="mt-4 flex flex-col break-all whitespace-normal">
+														<td className="flex items-center mb-2">
+															<FaRegCircleDot
+																fill="white"
+																size={20}
+																className="mt-[0.3rem] mr-3"
+															/>
+															<span className="text-2xl">Address:</span>
+														</td>
+														<td className="flex">
+															<FaArrowRight
+																className="mr-2 mt-[0.3rem]"
+																size={25}
+															/>{" "}
+															{profile?.address}
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</section>
+								</section>
+							) : (
+								<Loading />
+							)}
+						</>
+					) : (
+						<div className="w-full h-full flex items-center justify-center bg-gray-800">
+							<button
+								onClick={() => navigate("/setProfile")}
+								className="rounded-lg text-white bg-black flex justify-center items-center border-white border-2 text-xl font-semibold p-8"
+							>
+								Set your Profile
+							</button>
+						</div>
+					)}
+				</>
+			) : (
+				<Loading />
+			)}
+		</>
+	);
 };
 
 export default ProfileSection;
