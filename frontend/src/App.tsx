5977bf5e9d4bff5e289099d1786c17e92b289d25
Anshpatel2434 <anshpatel2434@gmail.com>
2024-07-06 05:11:48 +0530

    removed conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,30 +7,8 @@ import Products from "./pages/Products";
 import Loading from "./components/Loading";
 import AskPassword from "./components/AskPassword";
 import SetProfile from "./pages/SetProfile";
-<<<<<<< HEAD
-import About from "./pages/About";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/loading" element={<Loading />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/setProfile" element={<SetProfile />} />
-          <Route path="/askPassword" element={<AskPassword />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-=======
-import ItemInfo from "./components/ItemInfo";
+import About from "./pages/About";
 
 function App() {
 	return (
@@ -44,13 +22,12 @@ function App() {
 					<Route path="/profile" element={<Profile />} />
 					<Route path="/products" element={<Products />} />
 					<Route path="/loading" element={<Loading />} />
+					<Route path="/about" element={<About />} />
 					<Route path="/setProfile" element={<SetProfile />} />
 					<Route path="/askPassword" element={<AskPassword />} />
-					<Route path="/itemInfo/:id" element={<ItemInfo />} />
 				</Routes>
 			</BrowserRouter>
 		</>
 	);
->>>>>>> 4310172866657b4a18997801eacdb0642ecaffff
 }
 export default App;
